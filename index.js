ae19046703b71d8b528b276aa728f5445f084db5
VictorMunene <victor.munene@student.moringaschool.com>
2022-08-21 22:49:36 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+customerName();
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+setBestCustomer();
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+overwriteBestCustomer();
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer  = 'Mark';
+    leastFavoriteCustomer = 'Peter'
+    return leastFavoriteCustomer;
+}
+changeLeastFavoriteCustomer();
